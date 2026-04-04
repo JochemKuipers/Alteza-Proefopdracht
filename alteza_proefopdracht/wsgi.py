707c3407320efdd68146d0671e397a1a092f1bcb
Jochem Kuipers <jochem@kuipers.cc>
2026-04-04 14:00:41 +0200

    initial commit

---
+++ alteza_proefopdracht/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for alteza_proefopdracht project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alteza_proefopdracht.settings')
+
+application = get_wsgi_application()
